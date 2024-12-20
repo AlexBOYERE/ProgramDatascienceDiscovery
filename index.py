f691e2d4124a65f5d1c9e17be93f2c5d13c3eaae
AlexBOYERE <proalexandreboyere@gmail.com>
2024-12-20 16:14:21 +0100

    python

---
+++ index.py
@@ -0,0 +1,24 @@
+# Démarrage
+print('Python is running')
+
+# Définition de variable
+age = 25
+pi = 3.14159
+nom = 'Théo'
+is_student = True
+is_adult = age >= 18 # Retourne un boolean // Besoin que age soit un int
+
+# Affichage de variable
+print("Age :", age)
+print("Pi :", pi)
+print("Nom :", nom)
+print("Is student :", is_student)
+print("Is adult :", is_adult)
+
+# Exercice pratique n°1
+x = 8
+y = 3
+
+print("Somme :", x + y)
+print("Produit :", x * y)
+print("Puissance :", x ** y)
